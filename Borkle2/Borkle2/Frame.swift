11ec6973e249128e8269fbe5014c5164558e6cba
Mark Dalrymple <markd@borkware.com>
2025-03-21 20:43:49 -0400

    Sketches in layer data structure

--- Borkle2/Borkle2/Frame.swift
+++ Borkle2/Borkle2/Frame.swift
@@ -1,1 +1,30 @@
 import Foundation
+
+class Connection : Codable {
+    var start: Bubble.Identifier
+    var end: Bubble.Identifier
+    var label: String = ""
+    // connection style - e.g. arrows or not
+}
+
+class BubbleLocation: Codable {
+    var bubbleID: Bubble.Identifier
+    var frame: CGRect
+    var fillColor: RGB = RGB.white
+    // border style. stroke color
+}
+
+class Barrier: Codable {
+    var vertical = true
+    var location: NSInteger
+}
+
+// ----------
+
+class Frame: Codable {
+    var bubbleIDs: [Bubble.Identifier] = []
+    var locations: [BubbleLocation] = []
+    var connections: [Connection] = []
+
+    var barriers: [Barrier] = []
+}

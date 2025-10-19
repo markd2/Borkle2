c3ee7ad199e446034266ce0823f260f9d639b499
MarkD <markd@borkware.com>
2025-10-18 21:40:01 -0400

    Adds an xcode project to start bootstrapping the project

---
+++ Borkle2/Borkle2Tests/TestBedTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestBedTests.swift
+//  TestBedTests
+//
+//  Created by MarkD on 10/18/25.
+//
+
+import Testing
+@testable import TestBed
+
+struct TestBedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

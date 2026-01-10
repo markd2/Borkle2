c82672369ab6bf4f52549becc2b7f080ef5b3397
MarkD <markd@borkware.com>
2026-01-09 21:58:30 -0500

    migrate bubbles and soup to a class.  Adds verify

--- platforms/apple/Borkle2/Borkle2/BubbleSoup.swift
+++ platforms/apple/Borkle2/Borkle2/BubbleSoup.swift
@@ -1,5 +1,20 @@
 import Foundation
 
-struct BubbleSoup: Codable {
+class BubbleSoup: Codable {
     var bubbles: [Bubble] = []
+
+    func addBubble(_ bubble: Bubble) -> Int32 {
+        bubble.ID = Int32(bubbles.count)
+        bubbles.append(bubble)
+        return bubble.ID
+    }
+
+    func verify() {
+        for i in 0 ..< bubbles.count {
+            if bubbles[i].ID != i {
+                print("ID \(i) (vs \(bubbles[i].ID)) mismatch")
+            }
+        }
+        print("yay (\(bubbles.count) bubbles)")
+    }
 }

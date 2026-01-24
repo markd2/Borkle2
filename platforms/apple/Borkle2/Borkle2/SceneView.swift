27401899a88a62cd1d974cdb3844b6267960ea80
MarkD <markd@borkware.com>
2026-01-24 15:59:27 -0500

    Draw the geometry rectangles from the scene

--- platforms/apple/Borkle2/Borkle2/SceneView.swift
+++ platforms/apple/Borkle2/Borkle2/SceneView.swift
@@ -3,10 +3,29 @@
 import AppKit
 
 class SceneView: NSView {
+    // eventually might want a scene stack, if scenes can reference other
+    // scenes as bubbles
+    var scene: Scene! {
+        didSet {
+            needsDisplay = true
+        }
+    }
+    var soup: BubbleSoup!
+
+    override var isFlipped: Bool {
+        true
+    }
 
     override func draw(_ dirty_dirty_Rect: NSRect) {
         NSColor.white.set()
         bounds.fill()
+
+        if let scene {
+            NSColor.brown.set()
+            for geometry in scene.geometry {
+                geometry.bounds.frame()
+            }
+        }
 
         NSColor.black.set()
         bounds.frame()

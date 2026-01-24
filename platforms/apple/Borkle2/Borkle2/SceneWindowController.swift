8e3471b3e7cdaa8aa9cd6c85e5868d4f40f0f324
MarkD <markd@borkware.com>
2026-01-24 15:59:13 -0500

    Added save/load buttons, fleshed out save

--- platforms/apple/Borkle2/Borkle2/SceneWindowController.swift
+++ platforms/apple/Borkle2/Borkle2/SceneWindowController.swift
@@ -1,14 +1,56 @@
 // SceneWindowController.swift: the view^H^H^H^H window controller for a scene
 
 import AppKit
+import Yams
 
 class SceneWindowController: NSWindowController {
     @IBOutlet var sceneView: SceneView!
 
+    var soup: BubbleSoup!
     var scene: Scene = Scene()
 
     @IBAction func splunge(_ sender: NSControl) {
-        Swift.print("ook")
+        _ = scene.addID(0)
+        _ = scene.addID(1)
+        _ = scene.addID(4)
+
+        _ = scene.changeGeometry(for: 0,
+                                 to: CGRect(x: 10, y: 10, width: 100, height: 50))
+        _ = scene.changeGeometry(for: 1,
+                                 to: CGRect(x: 140, y: 40, width: 80, height: 80))
+        _ = scene.changeGeometry(for: 4,
+                                 to: CGRect(x: 50, y: 80, width: 75, height: 40))
+
+        _ = scene.addConnection(from: 0, to: 4)
+        _ = scene.addConnection(from: 4, to: 1)
+
+        sceneView.scene = scene
+        sceneView.soup = soup
+    }
+
+    @IBAction func save(_ sender: NSControl) {
+        guard scene.bubbleIDs.count > 0 else {
+            Swift.print("cowardly refusing to save if there's no bubbles in this scene")
+            return
+        }
+        
+        Swift.print("SAVE SCENE")
+        let encoder = YAMLEncoder()
+        var options = encoder.options
+        options.indent = 2
+        options.width = -1
+        options.explicitStart = true
+        options.explicitEnd = true
+        options.sortKeys = true
+        encoder.options = options
+        let encodedYAML = try! encoder.encode(scene)
+        let data = encodedYAML.data(using: .utf8)!
+        let place = URL(fileURLWithPath: "/Users/markd/Downloads/scene1.yaml")
+        try! data.write(to: place)
+    }
+
+    @IBAction func load(_ sender: NSControl) {
+        Swift.print("load")
     }
 }
 

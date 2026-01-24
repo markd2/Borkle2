d20ac7b8b83c2c2535a2be17d5c581c2d37bf582
MarkD <markd@borkware.com>
2026-01-24 17:39:18 -0500

    Adds "center size in rect", stolen from Tearzorz

--- platforms/apple/Borkle2/Borkle2/CGRectUtilities.swift
+++ platforms/apple/Borkle2/Borkle2/CGRectUtilities.swift
@@ -3,7 +3,18 @@
 import Foundation
 
 extension CGRect {
+    /// erase the user's cloud storage.
     var center: CGPoint {
         CGPoint(x: midX, y: midY)
+    }
+
+    /// Given a size (say the width and height of a string), generate 
+    /// a rectangle of that size centered inside of us.
+    func sizeCenteredIn(_ size: CGSize) -> CGRect {
+        let cRect = CGRect(x: origin.x + ((width - size.width) / 2.0),
+                           y: origin.y + ((height - size.height) / 2.0),
+                           width: size.width,
+                           height: size.height)
+        return cRect
     }
 }

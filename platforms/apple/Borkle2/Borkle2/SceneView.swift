f8aac6e3b7be159041f41b9572a73b5f3cb61e73
MarkD <markd@borkware.com>
2026-01-23 21:05:14 -0500

    Stubbing in sceneview and window controller

---
+++ platforms/apple/Borkle2/Borkle2/SceneView.swift
@@ -0,0 +1,14 @@
+// SceneView.swift: all the heavy lifting for the mind-map user interface
+
+import AppKit
+
+class SceneView: NSView {
+
+    override func draw(_ dirty_dirty_Rect: NSRect) {
+        NSColor.white.set()
+        bounds.fill()
+
+        NSColor.black.set()
+        bounds.frame()
+    }
+}

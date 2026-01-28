de160da2a1ea2a956283923e01b4a4cdfc96bab3
MarkD <markd@borkware.com>
2026-01-28 13:27:55 -0500

    inhale bubbles

--- platforms/apple/Borkle2/borkle1conv/main.swift
+++ platforms/apple/Borkle2/borkle1conv/main.swift
@@ -1,7 +1,7 @@
 // main.swift: borkle1-conv's driver program
 
 import Foundation
-
+import Yams
 
 let args = CommandLine.arguments
 
@@ -14,7 +14,15 @@ guard args.count == 3 else {
 let inputURL = URL(fileURLWithPath: args[1])
 let outputURL = URL(fileURLWithPath: args[2])
 
-print(inputURL, outputURL)
+// inputURL is a borkle document, which is a packge that has a
+// pair of yaml files, one for bubbles/soup, one for barriers.
+
+let inputYAMLURL = inputURL.appendingPathComponent("bubbles.yaml")
+let data = try! Data(contentsOf: inputYAMLURL, options: [])
+let decoder = YAMLDecoder()
+let oldBubbles = try! decoder.decode([B1Bubble].self, from: data)
+
+
 
 
 

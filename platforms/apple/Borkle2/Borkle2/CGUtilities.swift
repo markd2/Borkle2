3dde3af6777e9c5c96e72e9389ee2aa3f15e3d80
MarkD <markd@borkware.com>
2026-04-13 11:49:58 -0400

    Migrate some CGPoint utilities from Borkle1

--- platforms/apple/Borkle2/Borkle2/CGUtilities.swift
+++ platforms/apple/Borkle2/Borkle2/CGUtilities.swift
@@ -38,3 +38,26 @@ extension CGRect {
                height: size.height)
     }
 }
+
+extension CGPoint {
+    /// Make a new CGFloat with the given string, in the same manner as `Double(string)`
+    /// Takes a string of the format "3.1415,2.718"
+    init?(_ string: String) {
+        let components = string.split(separator: ",").map { String($0) }
+        if components.count != 2 { return nil }
+        
+        if let x = CGFloat(components[0]), let y = CGFloat(components[1]) {
+            self.init(x: x, y: y)
+        } else {
+            return nil
+        }
+    }
+
+    static public func - (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: right.x - left.x, y: right.y - left.y)
+    }
+
+    static public func + (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    }
+}

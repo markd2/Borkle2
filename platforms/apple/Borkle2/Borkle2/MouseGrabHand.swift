a161d5fcd47bbed5f316babc3875f510658dc745
MarkD <markd@borkware.com>
2026-04-13 11:50:50 -0400

    Grab the grab-hand scrolling mouse handler from borkle1

---
+++ platforms/apple/Borkle2/Borkle2/MouseGrabHand.swift
@@ -0,0 +1,33 @@
+/// Not Chicken Grab Foot, but Mouse Grab Hand.  Mouse handler for grab-hand scrolling.
+
+import Cocoa
+
+/// Mouse handler for grab-hand scrolling
+class MouseGrabHand: MouseHandler {
+    private var support: MouseSupport
+
+    var initialDragPoint: CGPoint!
+    var scrollOrigin: CGPoint!
+
+    var prefersWindowCoordinates: Bool { return true }
+    
+    init(withSupport support: MouseSupport) {
+        self.support = support
+    }
+
+    func start(at point: CGPoint, modifierFlags: NSEvent.ModifierFlags) {
+        initialDragPoint = point
+        scrollOrigin = support.currentScrollOffset
+    }
+    
+    func drag(to point: CGPoint, modifierFlags: NSEvent.ModifierFlags) {
+        let rawDelta = point - initialDragPoint
+        let flippedX = CGPoint(x: rawDelta.x, y: -rawDelta.y)
+        let newOrigin = scrollOrigin + flippedX
+
+        support.scroll(to: newOrigin)
+    }
+    
+    func finish(at: CGPoint, modifierFlags: NSEvent.ModifierFlags) {
+    }
+}

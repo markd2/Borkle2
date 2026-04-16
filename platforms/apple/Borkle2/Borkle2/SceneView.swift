e3aba52ca1df6daa4a6105ee6d6d9e6bacbaaed0
MarkD <markd@borkware.com>
2026-04-16 11:16:57 -0400

    move zoom out of scene view - it doesn't actually know about the scroller.

--- platforms/apple/Borkle2/Borkle2/SceneView.swift
+++ platforms/apple/Borkle2/Borkle2/SceneView.swift
@@ -12,8 +12,6 @@ class SceneView: NSView {
     }
     var soup: BubbleSoup!
 
-    var zoomLevel: Int = 100
-
     override var isFlipped: Bool {
         true
     }
@@ -28,7 +26,6 @@ class SceneView: NSView {
 
     // Event / user-interaction goodies
     var currentMouseHandler: MouseHandler?
-    var scrollOrigin: CGPoint? // !!! not sure this is actually used by anybody
 
     var spaceDown: Bool = false
     var currentCursor: Cursor = .arrow
@@ -180,7 +177,6 @@ extension SceneView {
         lastPoint = viewLocation
 
         defer {
-            scrollOrigin = nil
             currentMouseHandler = nil
         }
 

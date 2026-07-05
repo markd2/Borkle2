9af3788fe35093612b8a1a7cbe4f89914b42c005
MarkD <markd@borkware.com>
2026-07-05 15:52:01 -0400

    Adds search results machinery.

--- platforms/apple/Borkle2/Borkle2/Search.swift
+++ platforms/apple/Borkle2/Borkle2/Search.swift
@@ -1,13 +1,40 @@
 // Search - search through a soup for bubbles containing a particular string
 
+import AppKit
 
-struct SearchResult {
+enum SearchResult {
+    case titleRange(BubbleID, NSRange)
+    case bodyRange(BubbleID, NSRange)
+    case tagRange(BubbleID, String, NSRange)
 }
 
 class Searcher {
 
-    func search(for string: String, in soup: BubbleSoup) -> [SearchResult] {
-        return []
+    func ranges(of text: String, in string: String?) -> [NSRange] {
+        guard let string = string else { return [] }
+        return [NSRange(location: 1, length: 2)]
+    }
+
+    func search(for searchString: String,
+                in soup: BubbleSoup) -> [SearchResult] {
+        var results: [SearchResult] = []
+
+        for i in 0 ..< soup.bubbles.count {
+            let bubble = soup.bubbles[i]
+            let bubbleID = BubbleID(i)
+            
+            let titleRanges = ranges(of: searchString, in: bubble.title)
+            results += titleRanges.map { .titleRange(bubbleID, $0) }
+
+            let bodyRanges = ranges(of: searchString, in: bubble.body)
+            results += bodyRanges.map { .bodyRange(bubbleID, $0) }
+
+            for tag in bubble.tags ?? [] {
+                let tagRanges = ranges(of: searchString, in: tag)
+                results += tagRanges.map { SearchResult.tagRange(bubbleID, tag, $0) }
+            }
+        }
+        return results
     }
 }
 
